595a49fec52bdfe6290a5c63c24ff4b3133e4f07
Mohin Rana <ebrahimit49@gmail.com>
2023-11-24 12:38:57 +0600

    Firebase authenticaion done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,42 @@
 import { Link } from "react-router-dom";
 import SocilaLogin from "../../components/socilaLogin/SocilaLogin";
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import toast from "react-hot-toast";
 
 
 const Login = () => {
+    const {loginUser} = useAuth();
+    const {register,  handleSubmit, formState: { errors }, } = useForm()
+
+
+    const onSubmit = async (data) => {
+        const {email, password} = data || {};
+        const toastId = toast.loading("Loading");
+        try {
+            await loginUser(email, password)
+            toast.success("Login Success", {id: toastId})
+        } catch (error) {
+            toast.error("Somthing wrong your crediantials", {id: toastId})
+        }
+    }
+
+    const emailValidation = {
+        required : "Email fild is Required",
+        minLength: {
+            value: 5,
+            message: "Email charecter length must bee 5 charecter"
+        },
+        maxLength: {
+            value : 20,
+            message : "Email charecter limit is 20"
+        },
+        pattern: {
+            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            message : "Invalid email address"
+        }
+    }
+
     return (
         <section className=''>
             <div className='' >
@@ -12,15 +46,16 @@ const Login = () => {
                             <div className='px-5 py-5 md:px-10 md:pb-10 rounded-b-lg bg-secondary '>
                                 <div className='text-3xl pb-5 font-semibold text-gray-800'>Login </div>
                                 <div>
-                                    <form >
-                                       
+                                    <form onSubmit={handleSubmit(onSubmit)}>
                                         <div className='mb-4'>
                                             <label htmlFor="" className='mb-1 block font-medium text-gray-800'>Email</label>
-                                            <input type="email" name='email' placeholder='Email' className='px-3 w-full py-3  border border-gray-200 text-gray-700 rounded-md outline-none' />
+                                            <input type="email" name='email' {...register("email", emailValidation)} placeholder='Email' className='px-3 w-full py-3  border border-gray-200 text-gray-700 rounded-md outline-none' />
+                                            <p className="text-red-500 text-sm">{errors.email && errors.email.message }</p>
                                         </div>
                                         <div className='mb-5'>
                                             <label htmlFor="" className='mb-1 block font-medium text-gray-800'>Password</label>
-                                            <input type="password" name='password' placeholder='Password' className='px-3 w-full py-3  border border-gray-200 text-gray-700 rounded-md outline-none' />
+                                            <input type="password" name='password' {...register("password", { required: 'Password is required' })} placeholder='Password' className='px-3 w-full py-3  border border-gray-200 text-gray-700 rounded-md outline-none' />
+                                            <p className="text-red-500 text-sm">{errors.password && errors.password.message }</p>
                                         </div>
                                         <div className='mb-4 flex gap-5'>
                                             <button type='submit' className='px-5 py-3 rounded-md bg-purple-500 text-white font-medium w-full'>Sign In</button>
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { useState } from "react";
 
 
 const Header = () => {
+    const [isOpen , setIsOpen] = useState(false);
+    const {user,logOut} = useAuth();
+
+    const handleLogut = async () => {
+        await logOut()
+    }
     return (
         <>
             <header className="py-3 shadow">
@@ -20,7 +28,22 @@ const Header = () => {
                             </ul>
                         </div>
                         <div className="col-span-3 flex justify-end items-center">
-                            <button className="px-4 py-2 inline-block bg-purple-500 text-sm font-medium rounded text-white">Join Apply</button>
+                            {
+                                user?.email ? <>
+                                    <div className="relative">
+                                        <span className="cursor-pointer" onClick={() => setIsOpen(!isOpen) }>
+                                            <img className="w-10 h-10 rounded-full object-cover" src={user?.photoURL} alt="" />
+                                        </span>
+                                        <ul className={`absolute right-0 top-full bg-white w-[150px] border rounded-md ${isOpen ? 'block':'hidden'} `}>
+                                            <li><div className="py-4 bg-gray-200 w-full   text-center">{user?.displayName}</div></li>
+                                            <li><Link className="pl-4 inline-block py-1 mt-3 hover:text-purple-500" to={'/dashboard'}>Dashboard</Link></li>
+                                            <li><button onClick={handleLogut} className="pl-4 inline-block py-1 mb-3 hover:text-purple-500">Logout</button></li>
+                                        </ul>
+                                    </div>
+                                </> : 
+                                <button className="px-4 py-2 inline-block bg-purple-500 text-sm font-medium rounded text-white">Join Apply</button>
+                            }
+                           
                         </div>
                     </div>
                 </div>
